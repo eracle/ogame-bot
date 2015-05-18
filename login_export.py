979268db1800211fb749f717262947aaa61afe49
eracle <ercole.adeluca@gmail.com>
2015-05-19 01:44:30 +0200

    UNTRUSTY COMMIT

---
+++ login_export.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class Login(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://it.ogame.gameforge.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_login(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_id("loginBtn").click()
+        Select(driver.find_element_by_id("serverLogin")).select_by_visible_text("Fidis")
+        driver.find_element_by_id("usernameLogin").clear()
+        driver.find_element_by_id("usernameLogin").send_keys("ercole.adeluca@gmail.com")
+        driver.find_element_by_id("passwordLogin").clear()
+        driver.find_element_by_id("passwordLogin").send_keys("arkos1987?")
+        driver.find_element_by_id("loginSubmit").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
